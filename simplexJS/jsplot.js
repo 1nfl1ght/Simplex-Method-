049639c48aeda6f59498cee8d1119658565f2a57
1nfl1ght <80851826+1nfl1ght@users.noreply.github.com>
2021-04-16 22:52:59 +0300

    Update jsplot.js

--- simplexJS/jsplot.js
+++ simplexJS/jsplot.js
@@ -11,48 +11,88 @@ for (let i = 0; i < 16; i++) {
 
 let varArr = [];
 
-let variables = document.querySelectorAll('input[type="number"]');
-let varLength = allinputs.length;
+// let variables = document.querySelectorAll('input[type="number"]');
+// let varLength = variables.length;
 
-for (let i = 0; i < myLength; ++i) {
-    varArr.push(parseFloat(allinputs[i].value));
+// for (let i = 0; i < varLength; ++i) {
+//     varArr.push(parseFloat(variables[i].value));
+// }
+
+function feature(x, y, c) {
+  b = my_arr.map(
+      function(currentValue){
+          return ((c - x*currentValue)/y);
+      }
+  );
 }
 
 let b = [];
 
-function feature(x, y, b) {
-    b = my_arr.map(
-        function(currentValue){
-            return ((b - x*currentValue)/y);
+function run() {
+  let variables = document.querySelectorAll('input[type="number"]');
+  let varLength = variables.length;
+
+  for (let i = 0; i < varLength; ++i) {
+      varArr.push(parseFloat(variables[i].value));
+  }
+
+  
+
+  const labels = labelsx
+  const data = {
+  labels: labels,
+  datasets: [{
+      label: 'My First Dataset',
+      data: b,
+      borderColor: 'rgb(75, 192, 192)'
+  }]
+  };
+  function createAPlot() {
+    let ctx = document.querySelector('.myChart').getContext('2d');
+  let chart = new Chart(ctx, {
+  // Тип графика
+  type: 'line',
+  data: data,
+
+  // Настройки графиков
+  options: {
+    y: {
+      max: 25,
+      min: 0,
+      ticks: {
+            stepSize: 1
         }
-    );
-}
-
-const labels = labelsx
-const data = {
-labels: labels,
-datasets: [{
-    label: 'My First Dataset',
-    data: b,
-    borderColor: 'rgb(75, 192, 192)'
-}]
-};
-function createAPlot() {
-  let ctx = document.querySelector('.myChart').getContext('2d');
-let chart = new Chart(ctx, {
-// Тип графика
-type: 'line',
-data: data,
-
-// Настройки графиков
-options: {
-  y: {
-    max: 25,
-    min: 0,
-    ticks: {
-          stepSize: 1
-      }
+    }
+  }
+  });
   }
 }
-});
-}
+
+// const labels = labelsx
+// const data = {
+// labels: labels,
+// datasets: [{
+//     label: 'My First Dataset',
+//     data: b,
+//     borderColor: 'rgb(75, 192, 192)'
+// }]
+// };
+// function createAPlot() {
+//   let ctx = document.querySelector('.myChart').getContext('2d');
+// let chart = new Chart(ctx, {
+// // Тип графика
+// type: 'line',
+// data: data,
+
+// // Настройки графиков
+// options: {
+//   y: {
+//     max: 25,
+//     min: 0,
+//     ticks: {
+//           stepSize: 1
+//       }
+//   }
+// }
+// });
+// }
