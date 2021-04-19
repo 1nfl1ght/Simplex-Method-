e8cd057a9bae791e8d84f501de3d47453e008c38
1nfl1ght <80851826+1nfl1ght@users.noreply.github.com>
2021-04-19 21:37:57 +0300

    Update jsplot.js

--- simplexJS/jsplot.js
+++ simplexJS/jsplot.js
@@ -1,35 +1,102 @@
-// let variables = document.querySelectorAll('input[type="number"]');
-// let varLength = variables.length;
+function note_data() {
+  let variables = document.querySelectorAll('input[type="number"]');
+  let varLength = variables.length;
 
-// for (let i = 0; i < varLength; ++i) {
-//     varArr.push(parseFloat(variables[i].value));
-// }
+  let varArr = [];
 
+  window.xs = []
 
-// b = my_arr.map(
-//     function(currentValue){
-//         return ((c - x*currentValue)/y);
-//     }
-// );
+  for (let i = -5; i < 6; i++) {
+    xs.push(i);
+  }
+
+  for (let i = 0; i < varLength; ++i) {
+      varArr.push(parseFloat(variables[i].value));
+  }
+
+  let args1 = [];
+
+  for (let i = 0; i < 3; i++) {
+    args1.push(varArr[i]);
+  }
+
+  let args2 = [];
+
+  for (let i = 3; i < 6; i++) {
+    args2.push(varArr[i]);
+  }
+
+  let args3 = [];
+
+  for (let i = 6; i < 9; i++) {
+    args3.push(varArr[i]);
+  }
+
+  window.obj_func = [];
+
+  for (let i = 9; i < 11; i++) {
+    obj_func.push(varArr[i]);
+  }
+
+  window.y1Values = xs.map(
+    function(currentValue){
+        return ((-args1[0]*currentValue-args1[2])/args1[1]);
+    }
+  );
+
+  window.y2Values = xs.map(
+    function(currentValue){
+        return ((-args2[0]*currentValue-args2[2])/args2[1]);
+    }
+  );
+
+  window.y3Values = xs.map(
+    function(currentValue){
+        return ((-args3[0]*currentValue-args3[2])/args3[1]);
+    }
+  );
+  }
 
 
 function createAPlot() {
+  
+  note_data()
 
   TESTER = document.querySelector('#tester');
 
   var trace1 = {
-    x: [0, 1, 2, 3, 4, 5, 6, 7, 8],
-    y: [8, 7, 6, 5, 4, 3, 2, 1, 0],
+    x: xs,
+    y: y1Values,
     name: 'Первый график',
     type: 'scatter'
   };
   var trace2 = {
-    x: [0, 1, 2, 3, 4, 5, 6, 7, 8],
-    y: [0, 1, 2, 3, 4, 5, 6, 7, 8],
+    x: xs,
+    y: y2Values,
     name: 'Второй график',
     type: 'scatter'
   };
-  var data = [trace1, trace2];
+
+  var trace3 = {
+    x: xs,
+    y: y3Values,
+    name: 'третий',
+    type: 'scatter'
+  };
+
+  var trace4 = {
+    x: [0, obj_func[0]],
+    y: [0, obj_func[1]],
+    name: 'Целевая функция',
+    mode: 'lines',
+    line: {
+      dash: 'dot',
+      width: 4
+    }
+  };
+
+
+  var data = [trace1, trace2, trace3, trace4];
 
   var layout = {
     title: 'Графическое изображение решения',
@@ -48,8 +115,6 @@ function createAPlot() {
       linewidth: 6
     },
     yaxis: {
-      tick0: 0,
-      dtick: 1,
       showgrid: true,
       zeroline: true,
       showline: true,
