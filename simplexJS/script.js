c3b34ef28246e60e9ffa6d05f13c4ce3eaea4127
1nfl1ght <80851826+1nfl1ght@users.noreply.github.com>
2021-04-25 17:12:15 +0300

    Update script.js

--- simplexJS/script.js
+++ simplexJS/script.js
@@ -159,3 +159,89 @@ function calc() {
     // console.log(marks[0]);
     document.querySelector('#output').innerHTML = marks[0];
 }
+
+
+// Часть для мининммума
+function min_push_init(your_data) {
+    let p = 0;
+    for (let i = 0, j = 2; i < 3, j < your_data.length; i++, j+=3) {
+        t[i][1] = -your_data[j];
+    }
+    for (let i = 0; i < 3; i++) {
+        for (let j = 2; j < 4; j++) {
+            if (p == 2 || p == 5) {
+                t[i][j] = -initial_data[p+1];
+                p+=2;
+            }
+            else {
+                t[i][j] = -initial_data[p];
+                p++;
+            }
+        }
+    }
+}
+
+function min_objective_func() {
+    for (let i = 1, j = 9; i < 2, j < 11; i++, j++) {
+        k[i] = initial_data[j];
+    }
+}
+
+function min_marks_check(grades) {
+    for (let i = 0; i < grades.length; i++) {
+        if (grades[i] > 0) {
+            return true;
+        }
+    }
+}
+
+function min_reference_elem(matrix, grades) {
+    let score;
+    window.reference;
+    let min_reference;  // Будет возвращать значение опорного элемента
+    window.row_refer; // Строка с опорным элементом
+    for (let i = 0; i < matrix.length; i++) {
+        if (matrix[i][argmin(grades) + 1] < 0) {
+            min_reference = matrix[i][argmin(grades) + 1];
+            row_refer = i;
+            score = matrix[i][1] / min_reference;
+            break;
+        }
+    }
+    for (let i = 0; i < matrix.length; i++) {
+        if ((matrix[i][1]/matrix[i][argmin(grades) + 1] < score) && (matrix[i][1]/matrix[i][argmin(grades) + 1] < 0)) {
+            min_reference = matrix[i][argmin(grades) + 1];
+            row_refer = i;
+        }
+    }
+    reference = min_reference;
+    return reference;
+}
+
+function min_start() {
+    deltaJ(t);
+    while (min_marks_check(marks) == true) {
+       min_reference_elem(t, marks);
+       new_iteration(t);
+    //    console.log(t);
+    //    console.log(marks);
+    }
+}
+
+// Запуск программы
+function min_calc() {
+    k = ([0, 0, 0, 0, 0, 0]);
+    t = ([[0, 0, 0, 0, 1, 0, 0],
+         [0, 0, 0, 0, 0, 1, 0],
+         [0, 0, 0, 0, 0, 0, 1]])
+    write_init();
+    console.log(initial_data);
+    min_push_init(initial_data);
+    min_objective_func();
+    
+    min_start();
+    window.marks2 = [];
+    marks2 = marks.slice(0, marks.length);
+    // console.log(marks[0]);
+    document.querySelector('#output2').innerHTML = marks2[0];
+}
